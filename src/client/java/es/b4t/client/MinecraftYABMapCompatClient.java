18adccd32e8b64c996a50109baba72e7fc8a69ef
bates <florian@cegledi.net>
2026-05-30 14:11:25 +0200

    feat: implemented waypoint deletion and /clearmap command

--- src/client/java/es/b4t/client/MinecraftYABMapCompatClient.java
+++ src/client/java/es/b4t/client/MinecraftYABMapCompatClient.java
@@ -1,11 +1,19 @@
 package es.b4t.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.server.command.CommandManager;
 
 public class MinecraftYABMapCompatClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
+        CommandRegistrationCallback.EVENT.register(((dispatcher, registryAccess, environment) -> {
+            dispatcher.register(CommandManager.literal("clearmap").executes(context -> {
+                MapDevourer.EradicateCurrentMap(true);
 
+                return 1;
+            }));
+        }));
     }
 }
